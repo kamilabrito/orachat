c312c565213d4eb256c06e8059642f5ff773a0c6
kamilabrito <kamila.brito@gmail.com>
2017-07-25 15:27:31 -0700

    -app base classes -dagger injection classes -retrofit service class

---
+++ app/src/main/java/com/orainteractive/orachat/dagger/module/ApplicationModule.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *            http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.orainteractive.orachat.dagger.module;
+
+import android.content.Context;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by kamilabrito on 7/26/17.
+ */
+@Module
+public class ApplicationModule {
+
+    private String mBaseUrl;
+    private Context mContext;
+
+    public ApplicationModule(Context mContex, String mBaseUrl) {
+        this.mBaseUrl = mBaseUrl;
+        this.mContext = mContex;
+    }
+
+    @Singleton
+    @Provides
+    GsonConverterFactory provideGsonConverterFactory() {
+        return GsonConverterFactory.create();
+    }
+    @Singleton
+    @Provides
+    @Named("ok-1")
+    OkHttpClient provideOkHttpClient1() {
+        return new OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    @Named("ok-2")
+    OkHttpClient provideOkHttpClient2() {
+        return new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    RxJava2CallAdapterFactory provideRxJavaCallAdapterFactory() {
+        return RxJava2CallAdapterFactory.create();
+    }
+
+    @Singleton
+    @Provides
+    Retrofit provideRetrofit(@Named("ok-1") OkHttpClient client, GsonConverterFactory converterFactory, RxJava2CallAdapterFactory adapterFactory) {
+        return new Retrofit.Builder()
+                .baseUrl(mBaseUrl)
+                .addConverterFactory(converterFactory)
+                .addCallAdapterFactory(adapterFactory)
+                .client(client)
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    Context provideContext() {
+        return mContext;
+    }
+}
